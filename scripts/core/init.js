623de4685e6d78305e8000286c9f2d801b83f782
Felipe Ramos <felipe.ramos@osf.digital>
2022-01-15 22:47:29 -0300

    [#19] - Last improvements
    Last improvements before PR.

--- scripts/core/init.js
+++ scripts/core/init.js
@@ -4,30 +4,15 @@ import utils from '../../utils';
 
 function getDataFromDOM($this, mutableName, mutableType){
     const {throwError, error} = utils.logs;
-    const {checkType} = scripts.validation;
     let mutableValue;
 
     switch (mutableType) {
         case 'string': {
-            mutableValue = $this.attr('mutable-value') || $this.val() || $this.attr('value') || $this.text() || '';
+            mutableValue = grabString($this);
             break;
         }
         case 'number': {
-            let parsed = $this.attr('mutable-value') || $this.val() || $this.attr('value') || $this.text();
-
-            if(checkType(parsed) === 'string'){
-                let number = Number(parsed.replaceAll(' ', ''));
-
-                if(checkType(number) !== 'number') throwError(
-                    `The value provided isn't resulting in a number!`,
-                    `Please check the value mutable "${mutableName}.`
-                );
-
-                mutableValue = number;
-            }
-            break;
-        }
-        case 'number': {
+            mutableValue = grabNumber($this);
             break;
         }
         case 'array': {
@@ -47,11 +32,31 @@ function getDataFromDOM($this, mutableName, mutableType){
             throwError(
                 `The mutable type is incorrect! It's provided "${mutableType}". \nThe only mutable types allowed is:\n'string'\n'number'\n'object'\n'array'\n'button'\n'html'\n'component'\n`,
                 `Please check the value mutable "${mutableName}.`
-            )
+            );
         }
     }
 
     return mutableValue;
+}
+
+function grabString($this){
+    return $this.attr('mutable-value') || $this.val() || $this.attr('value') || $this.text() || '';
+}
+
+function grabNumber($this){
+    const {checkType} = scripts.validation;
+    let parsed = $this.attr('mutable-value') || $this.val() || $this.attr('value') || $this.text();
+
+    if(checkType(parsed) === 'string'){
+        let number = Number(parsed.replaceAll(' ', ''));
+
+        if(checkType(number) !== 'number') throwError(
+            `The value provided isn't resulting in a number!`,
+            `Please check the value mutable "${mutableName}.`
+        );
+
+        return number;
+    }
 }
 
 export default {
